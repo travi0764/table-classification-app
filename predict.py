882a8200145a5d1f4bdb034ca4dc8ef4f54558c5
Ravi Tiwari <travi0764@gmail.com>
2024-06-23 15:56:10 +0530

    download model from drive

--- predict.py
+++ predict.py
@@ -1,7 +1,23 @@
+import os
+import shutil
+import gdown
 import torch
 from PIL import Image
 from transformers import AutoModelForImageClassification, AutoImageProcessor
 
+DOWNLOAD_URL = f'https://drive.google.com/drive/folders/1z0kB1TWQOTMWvgOERGbE8jaW1tzo71tz?usp=sharing'
+# Path to save the downloaded model
+MODEL_PATH = 'model'
+
+def download_model():
+    if os.path.exists(MODEL_PATH):
+        shutil.rmtree(MODEL_PATH)
+    os.makedirs(MODEL_PATH)
+    
+    gdown.download_folder(url=DOWNLOAD_URL, output=MODEL_PATH, quiet=True, use_cookies=False)
+    print("Model folder downloaded successfully.")
+
+download_model()
 # Load your fine-tuned model (make sure to update the path and model loading mechanism)
 model_path = "model/vit-base"
 image_processor = AutoImageProcessor.from_pretrained(model_path)
